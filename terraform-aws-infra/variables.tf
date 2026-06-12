d0fc6e53762ba0f89ac2593b1ba3ef45dcd2ff2c
dillibabu55 <chdillibabu314@gmail.com>
2026-06-12 22:33:26 +0530

    Update variables.tf

--- terraform-aws-infra/variables.tf
+++ terraform-aws-infra/variables.tf
@@ -2,10 +2,10 @@ variable "aws_region" {
   default = "ap-south-1"
 }
 
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+variable "instance_type" {
+  default = "t2.micro"
 }
 
-variable "public_subnet_cidr" {
-  default = "10.0.1.0/24"
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
 }

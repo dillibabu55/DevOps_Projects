6ff74cd778d92b4c4525498add641cb4af117d3c
dillibabu55 <chdillibabu314@gmail.com>
2026-06-12 22:34:20 +0530

    Add security group, keypair, and EC2 modules

--- terraform-aws-infra/main.tf
+++ terraform-aws-infra/main.tf
@@ -1,6 +1,15 @@
-module "vpc" {
-  source = "./modules/vpc"
+module "security_group" {
+  source = "./modules/security-group"
+}
 
-  vpc_cidr           = var.vpc_cidr
-  public_subnet_cidr = var.public_subnet_cidr
+module "keypair" {
+  source          = "./modules/keypair"
+  public_key_path = var.public_key_path
+}
+
+module "ec2" {
+  source            = "./modules/ec2"
+  instance_type     = var.instance_type
+  security_group_id = module.security_group.sg_id
+  key_name          = module.keypair.key_name
 }
